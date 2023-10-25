f293de3da3f050288cdd3dc143ce79d90a3cd335
Levi Fairchild <119886172+Gmanbathwater@users.noreply.github.com>
2023-10-25 14:50:23 -0400

    added fluff basicly

--- cyberRangeUpdater.sh
+++ cyberRangeUpdater.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+echo What is your username
+read username
 cd ..
 cd ..
 cd ..
 cd ..
 cd ..
 cd home
-cd student
+cd $username
 mkdir CourseFiles
 cd CourseFiles
 mkdir CyberSecurity
@@ -13,7 +15,9 @@ cd CyberSecurity
 git clone https://github.com/cyber-org/documents
 cd ..
 cd ..
-if (( -e HeartlandCyberUpdater ))
+if [ -e HeartlandCyberUpdater ]
 then
+    echo if you recive an error here dont worry, check the student file to ensure that the updater correctly deleted itself. if it didnt dont worry, just be sure to use chmod rwx cyberRangeUpdater.sh next time you update
 	rm -rf HeartlandCyberUpdater
 fi
+echo check the student file to ensure that the updater correctly deleted itself
