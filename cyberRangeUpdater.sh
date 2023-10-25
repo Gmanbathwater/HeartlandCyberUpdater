1e85ccc9fa745b5e5bda696df064b2f13fcaa57d
Levi Fairchild <119886172+Gmanbathwater@users.noreply.github.com>
2023-10-25 15:21:45 -0400

    conditionals conditionals

--- cyberRangeUpdater.sh
+++ cyberRangeUpdater.sh
@@ -8,13 +8,35 @@ cd ..
 cd ..
 cd home
 cd $username
-mkdir CourseFiles
-cd CourseFiles
-mkdir CyberSecurity
-cd CyberSecurity
-git clone https://github.com/cyber-org/documents #change this to change updates
+if [ -e CourseFiles ]
+then 
+	cd CourseFiles
+ 	if [ -e CyberSecurity ]
+  	then
+   		cd CyberSecurity
+     	else
+      		mkdir CyberSecurity
+		cd CyberSecurity
+  	fi
+else
+	mkdir CourseFiles
+	cd CourseFiles
+	mkdir CyberSecurity
+	cd CyberSecurity
+fi
+if [ -e documents ]
+then
+	echo oops: there is nothing to update
+else
+	git clone https://github.com/cyber-org/documents #change this to change updates
+fi
 cd ..
 cd ..
+cd ..
+cd ..
+cd ..
+cd home
+cd $username
 if [ -e HeartlandCyberUpdater ]
 then
     echo if you recive an error here dont worry, check the student file to ensure that the updater correctly deleted itself. if it didnt dont worry, just be sure to use chmod rwx cyberRangeUpdater.sh next time you update
