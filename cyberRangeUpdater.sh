693b6d4c93fc068ac56a3527c73aacc868848a27
Levi Fairchild <119886172+Gmanbathwater@users.noreply.github.com>
2023-10-25 13:55:50 -0400

    Add files via upload

---
+++ cyberRangeUpdater.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd home
+cd student
+mkdir CourseFiles
+cd CourseFiles
+mkdir CyberSecurity
+cd CyberSecurity
+git clone https://github.com/cyber-org/documents
